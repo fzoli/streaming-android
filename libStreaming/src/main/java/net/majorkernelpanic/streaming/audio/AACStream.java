862473e7c3d1208456dcebf825533f3ce7c941e2
Zoltan Farkas <progfarkas@gmail.com>
2017-11-22 02:22:00 +0100

    rename

--- libStreaming/src/main/java/net/majorkernelpanic/streaming/audio/AACStream.java
+++ libStreaming/src/main/java/net/majorkernelpanic/streaming/audio/AACStream.java
@@ -18,16 +18,6 @@
 
 package net.majorkernelpanic.streaming.audio;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.lang.reflect.Field;
-import java.net.InetAddress;
-import java.nio.ByteBuffer;
-import net.majorkernelpanic.streaming.SessionBuilder;
-import net.majorkernelpanic.streaming.rtp.AACADTSPacketizer;
-import net.majorkernelpanic.streaming.rtp.AACLATMPacketizer;
-import net.majorkernelpanic.streaming.rtp.MediaCodecInputStream;
 import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
@@ -41,6 +31,18 @@ import android.os.Build;
 import android.os.Environment;
 import android.service.textservice.SpellCheckerService.Session;
 import android.util.Log;
+
+import net.majorkernelpanic.streaming.SessionBuilder;
+import net.majorkernelpanic.streaming.rtp.AACADTSPacketizer;
+import net.majorkernelpanic.streaming.rtp.AACLATMPacketizer;
+import net.majorkernelpanic.streaming.rtp.MediaCodecInputStream;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.lang.reflect.Field;
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
 
 /**
  * A class for streaming AAC from the camera of an android device using RTP.
@@ -297,7 +299,7 @@ public class AACStream extends AudioStream {
 			return;
 		}
 
-		final String TESTFILE = Environment.getExternalStorageDirectory().getPath()+"/spydroid-test.adts";
+		final String TESTFILE = Environment.getExternalStorageDirectory().getPath()+"/libstreaming-test.adts";
 
 		if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
 			throw new IllegalStateException("No external storage or external storage not ready !");
